5f2f33ecc4a1b54f131a949220d07c02b74f1fb1
cclarkes <connorclarkes@gmail.com>
2019-05-16 19:08:12 -0600

    added react router + navbar wireframe

---
+++ my-app/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/navbar.css";
+
+const Navbar = () => {
+    return(
+        <div className="nav">
+            <img id="logo" alt="random" src="https://picsum.photos/300/200/"/>
+            <h1 className="connor">Connor Clarkes - BreezeWay Digital</h1>
+        </div>
+    )
+}
+
+export default Navbar
